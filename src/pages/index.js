16c18d5efad00c5ffe49b3c1590fe3894b5e4325
tyler malin <tylermalin@gmail.com>
2019-10-02 22:11:21 -0700

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,10 +19,9 @@ const Home = (props) => {
       </Helmet>
       <div className="intro pb-4">
         <div className="container">
-          <h1>Serif - Gatsby Small Business Theme.</h1>
+          <h1>RDCL</h1>
           <p>
-            Multiple content types using Markdown and JSON sources. Responsive design and SCSS. This
-            is a beautiful and artfully designed starting theme.
+           We are RDCL
           </p>
         </div>
       </div>
